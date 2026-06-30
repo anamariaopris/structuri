cb71e2b5d185bc9baf323bd747936491f3daa520
Opris Ana <anaopris68@gmail.com>
2026-06-30 20:06:04 +0300

    user -  exemplu

--- Program.cs
+++ Program.cs
@@ -1,10 +1,38 @@
 ﻿
+using structuri;
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("test");
+        int a = 7;
+        User x= new User();
+
+        User y = new User();
+
+        User z = new User();
+
+        x.email = "test@gmail.com";
+        x.age = 23;
+        x.password = "123a";
+        x.isActive = true;
+        x.name = "Ana";
+
+        y.email = "test2@gmail.com";
+        y.age = 30;
+        y.password = "12a";
+        y.isActive = true;
+        y.name = "Maria";
+    
+
+        z.email = "test3@gmail.com";
+        z.age = 33;
+        z.password = "1aa";
+        z.isActive = true;
+        z.name = "test";
+
+
+
     }
 }
---
+++ User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace structuri
+{
+    //atribute si metode
+    internal class User
+    {
+
+        public String name;
+        public String email;
+        public String password;
+        public int age;
+        public bool isActive;
+
+    }
+}
