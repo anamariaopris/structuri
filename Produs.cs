ffc07e411c088638e2d779ac91b1e5d39179bddf
Opris Ana <anaopris68@gmail.com>
2026-06-30 20:57:44 +0300

    finished users

---
+++ Produs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace structuri
+{
+    internal class Produs
+    {
+        public String nume;
+        public Double pret;
+        public int stoc;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using structuri;
 using System;
 using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
@@ -9,12 +10,12 @@ class Program
     {
 
 
-        ex1();
+        ex4();
     }
 
     static void ex1()
     {
-        int a = 7;
+        
         User x = new User();
 
         User y = new User();
@@ -57,4 +58,142 @@ class Program
             Console.WriteLine("Userul cu varsta maxima este " + z.age);
         }
     }
+
+    static void ex2()
+    {
+        User x = new User();
+
+        User y = new User();
+
+        User z = new User();
+
+        x.email = "test@gmail.com";
+        x.age = 43;
+        x.password = "123a";
+        x.isActive = true;
+        x.name = "Ana";
+
+        y.email = "test2@gmail.com";
+        y.age = 30;
+        y.password = "12a";
+        y.isActive = true;
+        y.name = "Maria";
+
+
+        z.email = "test3@gmail.com";
+        z.age = 33;
+        z.password = "1aa";
+        z.isActive = true;
+        z.name = "test";
+
+        //cati useri sunt activi
+
+        int contorActivi = 0;
+
+        if (x.isActive)
+        {
+
+            contorActivi++;
+
+        }
+        if (y.isActive)
+        {
+            contorActivi++;
+        }
+        if (z.isActive)
+        {
+            contorActivi++;
+        }
+        Console.WriteLine("Useri activi" + contorActivi );
+
+    }
+
+    static void ex3()
+    {
+        User x = new User();
+
+        User y = new User();
+
+        User z = new User();
+
+        x.email = "test@gmail.com";
+        x.age = 43;
+        x.password = "123a";
+        x.isActive = true;
+        x.name = "Ana";
+
+        y.email = "test2@gmail.com";
+        y.age = 30;
+        y.password = "12a";
+        y.isActive = true;
+        y.name = "Maria";
+
+
+        z.email = "test3@gmail.com";
+        z.age = 33;
+        z.password = "1aa";
+        z.isActive = true;
+        z.name = "test";
+
+        //afiseaza varsta medie a celor 3 useri
+
+      double sumamedie = (x.age + y.age + z.age) / 3;
+
+
+        Console.WriteLine(sumamedie);
+
+    }
+
+    static void ex4()
+    {
+        User x = new User();
+
+        User y = new User();
+
+        User z = new User();
+
+        x.email = "test@gmail.com";
+        x.age = 43;
+        x.password = "123a";
+        x.isActive = true;
+        x.name = "Ana";
+
+        y.email = "test2@gmail.com";
+        y.age = 30;
+        y.password = "12a";
+        y.isActive = true;
+        y.name = "Maria";
+
+
+        z.email = "test3@gmail.com";
+        z.age = 33;
+        z.password = "1aa";
+        z.isActive = true;
+        z.name = "test";
+
+        //sunt de acceasi varsta?
+
+       
+        
+        if (x.age == y.age &&x.age== z.age)
+        {
+            Console.WriteLine("au varste identice");
+        }
+        else 
+        {
+            Console.WriteLine(" nu au aceeasi varsta");
+        }
+
+    }
+    static void ex5()
+    {
+        Produs x = new Produs
+
+       
+
+        //
+
+    }
+
+
 }
